06023edfbf2b4d72f4288b2381046cadc78e16ec
昕蒲 <89232015+shinnpuru@users.noreply.github.com>
2024-12-26 19:09:22 +0800

    Update build_environment.sh

--- build_environment.sh
+++ build_environment.sh
@@ -1,4 +1,4 @@
-python -m venv .\venv
-.\venv\Scripts\python -m pip config set global.index-url https://mirror.sjtu.edu.cn/pypi/web/simple
-.\venv\Scripts\python -m pip install -r .\requirements.txt
+python -m venv ./venv
+./venv/bin/python -m pip config set global.index-url https://mirror.sjtu.edu.cn/pypi/web/simple
+./venv/bin/python -m pip install -r ./requirements.txt
 pause
